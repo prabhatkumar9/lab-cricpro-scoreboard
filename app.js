3d247eb15602864a3758cb5f424a71325ac76367
prabhatkumar9 <prabhatvisvakarma9@gmail.com>
2020-03-01 11:10:20 +0530

    completed

---
+++ app.js
@@ -0,0 +1,87 @@
+function show1() {
+    document.getElementById("object1").style.display = "block";
+}
+
+function hide1() {
+    document.getElementById("object1").style.display = "none";
+}
+
+function show2() {
+    document.getElementById("object2").style.display = "block";
+}
+
+function hide2() {
+    document.getElementById("object2").style.display = "none";
+}
+
+function show3() {
+    document.getElementById("object3").style.display = "block";
+}
+
+function hide3() {
+    document.getElementById("object3").style.display = "none";
+}
+
+function show4() {
+    document.getElementById("object4").style.display = "block";
+}
+
+function hide4() {
+    document.getElementById("object4").style.display = "none";
+}
+
+function show5() {
+    document.getElementById("object5").style.display = "block";
+}
+
+function hide5() {
+    document.getElementById("object5").style.display = "none";
+}
+
+function show6() {
+    document.getElementById("object6").style.display = "block";
+}
+
+function hide6() {
+    document.getElementById("object6").style.display = "none";
+}
+
+function show7() {
+    document.getElementById("object7").style.display = "block";
+}
+
+function hide7() {
+    document.getElementById("object7").style.display = "none";
+}
+
+function show8() {
+    document.getElementById("object8").style.display = "block";
+}
+
+function hide8() {
+    document.getElementById("object8").style.display = "none";
+}
+
+function show9() {
+    document.getElementById("object9").style.display = "block";
+}
+
+function hide9() {
+    document.getElementById("object9").style.display = "none";
+}
+
+function show10() {
+    document.getElementById("object10").style.display = "block";
+}
+
+function hide10() {
+    document.getElementById("object10").style.display = "none";
+}
+
+function show11() {
+    document.getElementById("object11").style.display = "block";
+}
+
+function hide11() {
+    document.getElementById("object11").style.display = "none";
+}
